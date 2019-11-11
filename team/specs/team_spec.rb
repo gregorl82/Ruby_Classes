24df9185b0aa60480c31ab8bb156ef86ee3ca4c6
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-11 17:39:55 +0000

    Adds tests for Team class getters

--- team/specs/team_spec.rb
+++ team/specs/team_spec.rb
@@ -8,5 +8,20 @@ class TeamTests < MiniTest::Test
     team = Team.new("No Hope United",["Bob", "Mick", "Lee", "Dave", "Chas"],"Alex Ferguson")
   end
 
+  def test_name
+    team = Team.new("No Hope United",["Bob", "Mick", "Lee", "Dave", "Chas"],"Alex Ferguson")
+    assert_equal("No Hope United", team.name)
+  end
+
+  def test_players
+    team = Team.new("No Hope United",["Bob", "Mick", "Lee", "Dave", "Chas"],"Alex Ferguson")
+    expected_output = ["Bob", "Mick", "Lee", "Dave", "Chas"]
+    assert_equal(expected_output, team.players)
+  end
+
+  def test_coach
+    team = Team.new("No Hope United",["Bob", "Mick", "Lee", "Dave", "Chas"],"Alex Ferguson")
+    assert_equal("Alex Ferguson", team.coach)
+  end
 #
 end
