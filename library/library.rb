9f67b6259f7930aaf334a4e2791ae8bc71f34df3
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-11 20:51:32 +0000

    Adds tests and methods to add books and modify rental details

--- library/library.rb
+++ library/library.rb
@@ -7,11 +7,38 @@ class Library
   end
 
   def add_book(book_title)
-    @books.push(  {
+    @books.push({
     title: book_title,
     rental_details: {
      student_name: "",
      date: ""}})
+  end
+
+  def book_info(book_title)
+    for book in @books
+      if (book[:title] == book_title)
+        return book
+      end
+    end
+  else return "Book not found"
+  end
+
+  def rental_details(book_title)
+    for book in @books
+      if (book[:title] == book_title)
+        return book[:rental_details]
+      end
+    end
+    else return "Book not found"
+  end
+
+  def change_rental_details(book_title, name, due_date)
+    for book in @books
+      if (book[:title] == book_title)
+        book[:rental_details][:student_name] = name
+        book[:rental_details][:date] = due_date
+      end
+    end
   end
 
 #
--- library/specs/library_spec.rb
+++ library/specs/library_spec.rb
@@ -16,8 +16,39 @@ class LibraryTests < MiniTest::Test
   def test_add_book
     library = Library.new()
     library.add_book("the_great_gatsby")
-    expected_output = [{title: "the_great_gatsby", rental_details: {student_name: "", date: ""}}]
+    library.add_book("age_of_innocence")
+    expected_output = [{title: "the_great_gatsby", rental_details: {student_name: "", date: ""}},
+                      {title: "age_of_innocence", rental_details: {student_name: "", date: ""}}]
     assert_equal(expected_output, library.books)
+  end
+
+  def test_book_info__book_found
+    library = Library.new()
+    library.add_book("pride_and_prejudice")
+    expected_output = {title: "pride_and_prejudice", rental_details: {student_name: "", date: ""}}
+    assert_equal(expected_output, library.book_info("pride_and_prejudice"))
+  end
+
+  def test_book_info__book_not_found
+    library = Library.new()
+    library.add_book("pride_and_prejudice")
+    assert_equal("Book not found", library.book_info("sense_and_sensibility"))
+  end
+
+  def test_rental_details
+    library = Library.new()
+    library.add_book("wuthering_heights")
+    library.change_rental_details("wuthering_heights", "Jack", "06/12/2019")
+    expected_output = {student_name: "Jack", date: "06/12/2019"}
+    assert_equal(expected_output, library.rental_details("wuthering_heights"))
+  end
+
+  def test_change_rental_details
+    library = Library.new()
+    library.add_book("jane_eyre")
+    library.change_rental_details("jane_eyre", "Jennifer", "12/01/2020")
+    expected_output = {title: "jane_eyre", rental_details: {student_name: "Jennifer", date: "12/01/2020"}}
+    assert_equal(expected_output, library.book_info("jane_eyre"))
   end
 
 #
